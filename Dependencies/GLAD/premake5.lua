f4dd39976ebb01101974a7a41ea979f3e538a016
GamesTrap <GamesTrap@web.de>
2019-05-28 16:42:32 +0200

    Added Dependencies 05/28/2019 | 19w21a2 | 0.2.75

---
+++ Dependencies/GLAD/premake5.lua
@@ -0,0 +1,34 @@
+project "GLAD"
+    kind "StaticLib"
+    language "C"
+    systemversion "latest"
+    staticruntime "on"
+    cdialect "C11"
+    pic "on"
+
+    targetdir ("../../bin/" .. outputdir .. "/%{prj.group}/%{prj.name}")
+    objdir ("../../bin-int/" .. outputdir .. "/%{prj.group}/%{prj.name}")
+
+    files
+    {
+        "include/glad/glad.h",
+        "include/KHR/khrplatform.h",
+        "src/glad.c"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+        optimize "On"
+
+	filter "configurations:RelWithDebInfo"
+		runtime "Release"
+        optimize "On"
