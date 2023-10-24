05b6c2dc77b00e989b3bf428bd1c27c6a24d77a5
GamesTrap <GamesTrap@web.de>
2023-10-25 00:09:11 +0200

    GitHub Action Runner Windows small change in install script 10/25/2023 | 23w42b3

--- .github/runners/Windows/install_software.ps1
+++ .github/runners/Windows/install_software.ps1
@@ -1,15 +1,16 @@
-choco install -y git gh 7zip jq vcredist-all python
+choco install -y gh 7zip jq vcredist-all python
 choco install -y nodejs-lts --version=12.22.12
-choco install -y cmake.install --installargs "ADD_CMAKE_TO_PATH=System"
+choco install -y git.install --params "'/GitAndUnixToolsOnPath'"
+choco install -y cmake --installargs "ADD_CMAKE_TO_PATH=System"
 choco install -y visualstudio2022buildtools --package-parameters "--includeRecommended --add Microsoft.VisualStudio.Workload.VCTools --locale en-US" #Install Visual Studio 2022 Build Tools (C++ Workload)
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
-$GitBashPath = "C:\Program Files\git\bin"
-$CMakePath = "C:\Program Files\CMake\bin"
-$7zPath = "C:\Program Files\7-Zip"
-[Environment]::SetEnvironmentVariable("PATH", $env:PATH + $GitBashPath, "Machine") #Add bash.exe to PATH
-[Environment]::SetEnvironmentVariable("PATH", $env:PATH + $CMakePath, "Machine") #Add cmake.exe to PATH
-[Environment]::SetEnvironmentVariable("PATH", $env:PATH + $7zPath, "Machine") #Add 7z.exe to PATH
+$GitBashPath = ";C:\Program Files\Git\bin;"
+$CMakePath = ";C:\Program Files\CMake\bin;"
+$7zPath = ";C:\Program Files\7-Zip;"
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $GitBashPath, "Machine") #Add bash.exe to PATH
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $CMakePath, "Machine") #Add cmake.exe to PATH
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $7zPath, "Machine") #Add 7z.exe to PATH
 
 #Create alias for node12
 if (!(Test-Path $PROFILE))
