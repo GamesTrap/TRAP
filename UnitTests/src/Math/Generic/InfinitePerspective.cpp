5687b8bbc8a158053a979e130081e7f7fcdeb9e4
GamesTrap <GamesTrap@web.de>
2023-11-04 18:33:59 +0100

    Fixed glitching of gizmos when using infinite far plane projection matrix 11/04/2023 | 23w44a1

--- UnitTests/src/Math/Generic/InfinitePerspective.cpp
+++ UnitTests/src/Math/Generic/InfinitePerspective.cpp
@@ -12,17 +12,17 @@ namespace
     {
         {
             constexpr auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(45.0f), 1.0f, 0.1f);
-            constexpr TRAP::Math::tMat4<T> expected(2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, 2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.2f, 0.0f);
+            constexpr TRAP::Math::tMat4<T> expected(2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, 2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.1f, 0.0f);
             static_assert(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
         {
             constexpr auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(90.0f), 16.0f / 9.0f, 0.01f);
-            constexpr TRAP::Math::tMat4<T> expected(0.562500f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.02f, 0.0f);
+            constexpr TRAP::Math::tMat4<T> expected(0.562500f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.01f, 0.0f);
             static_assert(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
         {
             constexpr auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(30.0f), 2.0f, 1.0f);
-            constexpr TRAP::Math::tMat4<T> expected(1.866025f, 0.0f, 0.0f, 0.0f, 0.0f, 3.732051f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -2.0f, 0.0f);
+            constexpr TRAP::Math::tMat4<T> expected(1.866025f, 0.0f, 0.0f, 0.0f, 0.0f, 3.732051f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f);
             static_assert(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
     }
@@ -33,17 +33,17 @@ namespace
     {
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(45.0f), 1.0f, 0.1f);
-            static constexpr TRAP::Math::tMat4<T> expected(2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, 2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.2f, 0.0f);
+            static constexpr TRAP::Math::tMat4<T> expected(2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, 2.414213f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.1f, 0.0f);
             REQUIRE(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(90.0f), 16.0f / 9.0f, 0.01f);
-            static constexpr TRAP::Math::tMat4<T> expected(0.562500f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.02f, 0.0f);
+            static constexpr TRAP::Math::tMat4<T> expected(0.562500f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -0.01f, 0.0f);
             REQUIRE(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(TRAP::Math::Radians<T>(30.0f), 2.0f, 1.0f);
-            static constexpr TRAP::Math::tMat4<T> expected(1.866025f, 0.0f, 0.0f, 0.0f, 0.0f, 3.732051f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -2.0f, 0.0f);
+            static constexpr TRAP::Math::tMat4<T> expected(1.866025f, 0.0f, 0.0f, 0.0f, 0.0f, 3.732051f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f);
             REQUIRE(TRAP::Math::All(TRAP::Math::Equal(p, expected, T(0.000001f))));
         }
     }
@@ -61,12 +61,12 @@ namespace
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(min, min, min);
             REQUIRE(TRAP::Math::Any(TRAP::Math::IsNaN(std::get<0>(p))));
-            REQUIRE(TRAP::Math::Any(TRAP::Math::IsInf(std::get<3>(p))));
+            // REQUIRE(TRAP::Math::Any(TRAP::Math::IsInf(std::get<3>(p))));
         }
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(max, max, max);
             REQUIRE(TRAP::Math::Any(TRAP::Math::IsNaN(std::get<0>(p))));
-            REQUIRE(TRAP::Math::Any(TRAP::Math::IsInf(std::get<3>(p))));
+            // REQUIRE(TRAP::Math::Any(TRAP::Math::IsInf(std::get<3>(p))));
         }
         {
             const auto p = TRAP::Math::InfinitePerspective<T>(inf, 1.0f, 0.1f);
