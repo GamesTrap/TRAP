e06302f7bd4ec7a5aeece6358e78cc843517f3a9
GamesTrap <GamesTrap@web.de>
2023-11-26 12:19:35 +0100

    Updated, rebuild and deployed GitHub Actions Runners 11/26/2023 | 23w47c2

--- .github/runners/Windows/install_software.ps1
+++ .github/runners/Windows/install_software.ps1
@@ -1,5 +1,4 @@
 choco install -y gh 7zip jq vcredist-all python
-choco install -y nodejs-lts --version=12.22.12
 choco install -y git.install --params "'/GitAndUnixToolsOnPath'"
 choco install -y cmake --installargs "ADD_CMAKE_TO_PATH=System"
 choco install -y visualstudio2022buildtools --package-parameters "--includeRecommended --add Microsoft.VisualStudio.Workload.VCTools --locale en-US" #Install Visual Studio 2022 Build Tools (C++ Workload)
@@ -11,14 +10,5 @@ $7zPath = ";C:\Program Files\7-Zip;"
 [Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $GitBashPath, "Machine") #Add bash.exe to PATH
 [Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $CMakePath, "Machine") #Add cmake.exe to PATH
 [Environment]::SetEnvironmentVariable("PATH", $env:PATH.TrimEnd(';') + $7zPath, "Machine") #Add 7z.exe to PATH
-
-#Create alias for node12
-if (!(Test-Path $PROFILE))
-{
-    New-Item -Type File -Path $PROFILE -Force
-}
-Set-Content -Path $PROFILE -Value "Set-Alias -Name node12 -Value node"
-Invoke-Item $PROFILE
-Set-Alias -Name node12 -Value node
 
 refreshenv
--- TRAP/src/Core/Base.h
+++ TRAP/src/Core/Base.h
@@ -129,7 +129,7 @@ enum class ProfileSystems : u32
 //-------------------------------------------------------------------------------------------------------------------//
 
 /// @brief TRAP version number created with TRAP_MAKE_VERSION
-inline constexpr TRAP::SemanticVersion<0, 10, 9> TRAP_VERSION{};
+inline constexpr TRAP::SemanticVersion<0, 10, 10> TRAP_VERSION{};
 
 //-------------------------------------------------------------------------------------------------------------------//
 
