2a151e64595a888a69659cca3d94e5f4a7004c12
GamesTrap <GamesTrap@web.de>
2024-01-03 06:26:35 +0100

    Backported C++23 container range insert, append and assign functions (p1206r7) 01/03/2023 | 24w01a1

--- TRAP/src/Core/Backports.h
+++ TRAP/src/Core/Backports.h
@@ -197,4 +197,60 @@ namespace std
 
 #endif /*__cpp_lib_debugging*/
 
+#if __cpp_lib_containers_ranges < 202202L
+
+template<typename Range, typename T>
+concept ContainerCompatibleRange = std::ranges::input_range<Range> && std::convertible_to<std::ranges::range_reference_t<Range>, T>;
+
+namespace INTERNAL
+{
+    template<typename Container>
+    concept ContainerHasInsertMemberFunction = requires(Container c, typename Container::iterator it)
+    {
+        {
+            c.insert(it, c.begin(), c.end())
+        } -> std::same_as<typename Container::iterator>;
+    };
+
+    template<typename Container>
+    concept ContainerHasEndIterator = requires(Container c) {c.end();};
+
+    template<typename Container>
+    concept ContainerHasAssignMemberFunction = requires(Container c)
+    {
+        {
+            c.assign(c.begin(), c.end())
+        } -> std::same_as<void>;
+    };
+}
+
+template<typename Container, ContainerCompatibleRange<typename Container::value_type> Range>
+requires INTERNAL::ContainerHasInsertMemberFunction<Container>
+constexpr typename Container::iterator ContainerInsertRange(Container& container, typename Container::const_iterator position, Range&& range)
+{
+    return container.insert(position, std::ranges::begin(range), std::ranges::end(range));
+}
+
+template<typename Container, ContainerCompatibleRange<typename Container::value_type> Range>
+requires INTERNAL::ContainerHasEndIterator<Container>
+constexpr void ContainerAppendRange(Container& container, Range&& range)
+{
+    ContainerInsertRange(container, container.end(), std::forward<Range>(range));
+}
+
+template<typename Container, ContainerCompatibleRange<typename Container::value_type> Range>
+requires INTERNAL::ContainerHasAssignMemberFunction<Container>
+constexpr void ContainerAssignRange(Container& container, Range&& range)
+{
+    container.assign(std::ranges::begin(range), std::ranges::end(range));
+}
+
+#else
+
+#warning "Backported std::<Container>::assign_range() should be removed!"
+#warning "Backported std::<Container>::insert_range() should be removed!"
+#warning "Backported std::<Container>::append_range() should be removed!"
+
+#endif
+
 #endif /*TRAP_BACKPORTS_H*/
