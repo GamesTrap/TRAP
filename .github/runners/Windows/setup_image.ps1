d8e975659713f7af18ae605ee93268d771d4d56b
GamesTrap <GamesTrap@web.de>
2024-01-14 02:51:32 +0100

    Added docker files for Gitea Actions Runners 01/14/2023 | 24w02b1

--- .github/runners/Windows/setup_image.ps1
+++ .github/runners/Windows/setup_image.ps1
@@ -4,5 +4,4 @@
 & .\install_choco.ps1 -Wait #Install Chocolatey
 & .\install_software.ps1 #Install Software
 & .\disable-jitdebugger.ps1 #Disable JIT Debuggers
-Invoke-WebRequest -Uri "https://github.com/actions/runner/releases/download/v$env:GH_RUNNER_VERSION/actions-runner-win-x64-$env:GH_RUNNER_VERSION.zip" -OutFile "actions-runner.zip"; Expand-Archive -Path "actions-runner.zip" -DestinationPath '.'; Remove-Item "actions-runner.zip" -Force #Download GitHub Action Runner
 & .\finalize-image.ps1 #Finalize image
