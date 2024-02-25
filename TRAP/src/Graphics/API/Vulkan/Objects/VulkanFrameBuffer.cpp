74c589afe18adb4edfc8d350e529a7517ed23682
GamesTrap <GamesTrap@web.de>
2024-02-26 00:51:39 +0100

    VulkanFrameBuffer Refactored constructor 02/26/2024 | 24w09a1

--- TRAP/src/Graphics/API/Vulkan/Objects/VulkanFrameBuffer.cpp
+++ TRAP/src/Graphics/API/Vulkan/Objects/VulkanFrameBuffer.cpp
@@ -8,6 +8,80 @@
 #include "Graphics/API/Vulkan/VulkanCommon.h"
 #include "Graphics/API/Vulkan/Objects/VulkanTexture.h"
 
+namespace
+{
+	[[nodiscard]] std::vector<VkImageView> GetImageViews(const TRAP::Graphics::API::VulkanRenderer::FrameBufferDesc& desc)
+	{
+		const u32 colorAttachmentCount = NumericCast<u32>(desc.RenderTargets.size());
+		const u32 depthAttachmentCount = desc.DepthStencil ? 1 : 0;
+		const u32 shadingRateAttachmentCount = desc.ShadingRate ? 1 : 0;
+
+		std::vector<VkImageView> imageViews{};
+		imageViews.reserve(colorAttachmentCount + depthAttachmentCount + shadingRateAttachmentCount);
+
+		//Color
+		for(usize i = 0; i < desc.RenderTargets.size(); i++)
+		{
+			const auto rTarget = std::dynamic_pointer_cast<TRAP::Graphics::API::VulkanRenderTarget>(desc.RenderTargets[i]);
+
+			if(desc.ColorMipSlices.empty() && desc.ColorArraySlices.empty())
+				imageViews.emplace_back(rTarget->GetVkImageView());
+			else
+			{
+				u32 handle = 0;
+				if (!desc.ColorMipSlices.empty())
+				{
+					if (!desc.ColorArraySlices.empty())
+						handle = desc.ColorMipSlices[i] * desc.RenderTargets[i]->GetArraySize() + desc.ColorArraySlices[i];
+					else
+						handle = desc.ColorMipSlices[i];
+				}
+				else if (!desc.ColorArraySlices.empty())
+					handle = desc.ColorArraySlices[i];
+
+				imageViews.emplace_back(rTarget->GetVkImageViewSlices()[handle]);
+			}
+		}
+
+		//Depth/Stencil
+		if(desc.DepthStencil)
+		{
+			const auto rTarget = std::dynamic_pointer_cast<TRAP::Graphics::API::VulkanRenderTarget>(desc.DepthStencil);
+
+			if(desc.DepthMipSlice == std::numeric_limits<u32>::max() &&
+			   desc.DepthArraySlice == std::numeric_limits<u32>::max())
+				imageViews.emplace_back(rTarget->GetVkImageView());
+			else
+			{
+				u32 handle = 0;
+				if (desc.DepthMipSlice != std::numeric_limits<u32>::max())
+				{
+					if (desc.DepthArraySlice != std::numeric_limits<u32>::max())
+						handle = desc.DepthMipSlice * desc.DepthStencil->GetArraySize() + desc.DepthArraySlice;
+					else
+						handle = desc.DepthMipSlice;
+				}
+				else if (desc.DepthArraySlice != std::numeric_limits<u32>::max())
+					handle = desc.DepthArraySlice;
+
+				imageViews.emplace_back(rTarget->GetVkImageViewSlices()[handle]);
+			}
+		}
+
+		//Shading rate
+		if(desc.ShadingRate)
+		{
+			const auto rTarget = std::dynamic_pointer_cast<TRAP::Graphics::API::VulkanRenderTarget>(desc.ShadingRate);
+
+			imageViews.emplace_back(rTarget->GetVkImageView());
+		}
+
+		return imageViews;
+	}
+}
+
+//-------------------------------------------------------------------------------------------------------------------//
+
 TRAP::Graphics::API::VulkanFrameBuffer::VulkanFrameBuffer(TRAP::Ref<VulkanDevice> device,
                                                           const VulkanRenderer::FrameBufferDesc& desc)
 	: m_device(std::move(device))
@@ -20,13 +94,9 @@ TRAP::Graphics::API::VulkanFrameBuffer::VulkanFrameBuffer(TRAP::Ref<VulkanDevice
 	TP_DEBUG(Log::RendererVulkanFrameBufferPrefix, "Creating FrameBuffer");
 #endif /*VERBOSE_GRAPHICS_DEBUG*/
 
-	const u32 colorAttachmentCount = NumericCast<u32>(desc.RenderTargets.size());
-	const u32 depthAttachmentCount = desc.DepthStencil ? 1 : 0;
-	const u32 shadingRateAttachmentCount = desc.ShadingRate ? 1 : 0;
-
-	if(colorAttachmentCount != 0u)
+	if(!desc.RenderTargets.empty())
 	{
-		const Ref<VulkanRenderTarget> renderTarget = std::dynamic_pointer_cast<VulkanRenderTarget>(desc.RenderTargets[0]);
+		const Ref<RenderTarget> renderTarget = desc.RenderTargets[0];
 		m_width = renderTarget->GetWidth();
 		m_height = renderTarget->GetHeight();
 		if (!desc.ColorArraySlices.empty())
@@ -34,7 +104,7 @@ TRAP::Graphics::API::VulkanFrameBuffer::VulkanFrameBuffer(TRAP::Ref<VulkanDevice
 		else
 			m_arraySize = renderTarget->GetArraySize();
 	}
-	else if(depthAttachmentCount != 0u)
+	else if(desc.DepthStencil != nullptr)
 	{
 		m_width = desc.DepthStencil->GetWidth();
 		m_height = desc.DepthStencil->GetHeight();
@@ -45,81 +115,13 @@ TRAP::Graphics::API::VulkanFrameBuffer::VulkanFrameBuffer(TRAP::Ref<VulkanDevice
 	}
 	else
 	{
-		TRAP_ASSERT(false, "VulkanFrameBuffer(): No color or depth attachments");
+		TRAP_ASSERT(false, "VulkanFrameBuffer(): No color or depth/stencil attachments");
 	}
 
-	if ((colorAttachmentCount != 0u) && desc.RenderTargets[0]->GetDepth() > 1)
+	if (!desc.RenderTargets.empty() && desc.RenderTargets[0]->GetDepth() > 1)
 		m_arraySize = desc.RenderTargets[0]->GetDepth();
 
-	const u32 attachmentCount = colorAttachmentCount + depthAttachmentCount + shadingRateAttachmentCount;
-
-	std::vector<VkImageView> imageViews(attachmentCount);
-	auto iterAttachments = imageViews.begin();
-
-	//Color
-	for(usize i = 0; i < desc.RenderTargets.size(); i++)
-	{
-		const Ref<VulkanRenderTarget> rTarget = std::dynamic_pointer_cast<VulkanRenderTarget>(desc.RenderTargets[i]);
-		if(desc.ColorMipSlices.empty() && desc.ColorArraySlices.empty())
-		{
-			*iterAttachments = rTarget->GetVkImageView();
-			++iterAttachments;
-		}
-		else
-		{
-			u32 handle = 0;
-			if (!desc.ColorMipSlices.empty())
-			{
-				if (!desc.ColorArraySlices.empty())
-					handle = desc.ColorMipSlices[i] * desc.RenderTargets[i]->GetArraySize() +
-					         desc.ColorArraySlices[i];
-				else
-					handle = desc.ColorMipSlices[i];
-			}
-			else if (!desc.ColorArraySlices.empty())
-				handle = desc.ColorArraySlices[i];
-
-			*iterAttachments = rTarget->GetVkImageViewSlices()[handle];
-			++iterAttachments;
-		}
-	}
-
-	//Depth/Stencil
-	if(desc.DepthStencil)
-	{
-		const Ref<VulkanRenderTarget> rTarget = std::dynamic_pointer_cast<VulkanRenderTarget>(desc.DepthStencil);
-		if(desc.DepthMipSlice == std::numeric_limits<u32>::max() &&
-		   desc.DepthArraySlice == std::numeric_limits<u32>::max())
-		{
-			*iterAttachments = rTarget->GetVkImageView();
-			++iterAttachments;
-		}
-		else
-		{
-			u32 handle = 0;
-			if (desc.DepthMipSlice != std::numeric_limits<u32>::max())
-			{
-				if (desc.DepthArraySlice != std::numeric_limits<u32>::max())
-					handle = desc.DepthMipSlice * desc.DepthStencil->GetArraySize() + desc.DepthArraySlice;
-				else
-					handle = desc.DepthMipSlice;
-			}
-			else if (desc.DepthArraySlice != std::numeric_limits<u32>::max())
-				handle = desc.DepthArraySlice;
-
-			*iterAttachments = rTarget->GetVkImageViewSlices()[handle];
-			++iterAttachments;
-		}
-	}
-
-	//Shading rate
-	if(desc.ShadingRate)
-	{
-		const Ref<VulkanRenderTarget> rTarget = std::dynamic_pointer_cast<VulkanRenderTarget>(desc.ShadingRate);
-
-		*iterAttachments = rTarget->GetVkImageView();
-		++iterAttachments;
-	}
+	const std::vector<VkImageView> imageViews = GetImageViews(desc);
 
 	const VkFramebufferCreateInfo info = VulkanInits::FramebufferCreateInfo(desc.RenderPass->GetVkRenderPass(),
 	                                                                        imageViews, m_width,
