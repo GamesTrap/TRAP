cc04de4d9a0ed1a0e33e3283912e26a904b238d8
GamesTrap <GamesTrap@web.de>
2024-10-10 10:23:39 +0200

    UnitTests Partially re-enabled UUID Tests 10/10/2024 | 24w41a2

--- UnitTests/src/Utils/UUID.cpp
+++ UnitTests/src/Utils/UUID.cpp
@@ -5,12 +5,12 @@
 #include <string>
 #include <string_view>
 
-// namespace
-// {
-//     constexpr TRAP::Utils::UUID testUUID{0xABu, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD};
-//     constexpr std::string_view testUUIDStr = "abcdefab-cdef-abcd-ef01-23456789abcd";
-//     constexpr std::string_view testUUIDStr1 = "abcdefabcdefabcdef0123456789abcd";
-// }
+namespace
+{
+    constexpr TRAP::Utils::UUID testUUID{0xABu, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD};
+    constexpr std::string_view testUUIDStr = "abcdefab-cdef-abcd-ef01-23456789abcd";
+    constexpr std::string_view testUUIDStr1 = "abcdefabcdefabcdef0123456789abcd";
+}
 
 //-------------------------------------------------------------------------------------------------------------------//
 //-------------------------------------------------------------------------------------------------------------------//
@@ -29,22 +29,22 @@
 //     }
 // }
 
-// TEST_CASE("TRAP::Utils::UUIDFromString()", "[utils][uuid]")
-// {
-//     SECTION("Valid")
-//     {
-//         STATIC_REQUIRE(TRAP::Utils::UUIDFromString(testUUIDStr) == testUUID);
-//         STATIC_REQUIRE(TRAP::Utils::UUIDFromString(testUUIDStr1) == testUUID);
-//     }
+TEST_CASE("TRAP::Utils::UUIDFromString()", "[utils][uuid]")
+{
+    SECTION("Valid")
+    {
+        STATIC_REQUIRE(TRAP::Utils::UUIDFromString(testUUIDStr) == testUUID);
+        STATIC_REQUIRE(TRAP::Utils::UUIDFromString(testUUIDStr1) == testUUID);
+    }
 
-//     SECTION("Invalid")
-//     {
-//         STATIC_REQUIRE(TRAP::Utils::UUIDFromString("h0000000000000000000000000000xyz") == TRAP::Utils::EMPTY_UUID);
-//     }
+    SECTION("Invalid")
+    {
+        STATIC_REQUIRE(TRAP::Utils::UUIDFromString("h0000000000000000000000000000xyz") == TRAP::Utils::EMPTY_UUID);
+    }
 
-//     SECTION("Empty")
-//     {
-//         STATIC_REQUIRE(TRAP::Utils::UUIDFromString("") == TRAP::Utils::EMPTY_UUID);
-//         STATIC_REQUIRE(TRAP::Utils::UUIDFromString("00000000-0000-0000-0000-000000000000") == TRAP::Utils::EMPTY_UUID);
-//     }
-// }
+    SECTION("Empty")
+    {
+        STATIC_REQUIRE(TRAP::Utils::UUIDFromString("") == TRAP::Utils::EMPTY_UUID);
+        STATIC_REQUIRE(TRAP::Utils::UUIDFromString("00000000-0000-0000-0000-000000000000") == TRAP::Utils::EMPTY_UUID);
+    }
+}
