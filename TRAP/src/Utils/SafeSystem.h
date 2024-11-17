956c471c9f93c5b98350202b3c6434d0a6b924d3
GamesTrap <GamesTrap@web.de>
2024-11-17 14:39:22 +0100

    Utils Added documentation for SafeSystem() 11/17/2024 | 24w46b1

--- TRAP/src/Utils/SafeSystem.h
+++ TRAP/src/Utils/SafeSystem.h
@@ -8,7 +8,14 @@
 
 namespace TRAP::Utils
 {
-    //TODO Document
+    /// @brief Safe replacement for system()/shell calls without invoking a command processor (ENV33-C).
+    /// @param program Executable to run. Example: "explorer", "xdg-open".
+    /// @param args Arguments for the executable.
+    /// @param waitForChild Whether to wait for the called executable to exit or not.
+    /// @return If waitForChild is false then void on successful invocation,
+    ///         if waitForChild is true then void on successful invocation and normal exit of the executable.
+    ///         String containing an error description otherwise.
+    /// @threadsafe
     [[nodiscard]] TRAP::Expected<void, std::string> SafeSystem(const std::string& program, const std::vector<std::string>& args, bool waitForChild);
 }
 
