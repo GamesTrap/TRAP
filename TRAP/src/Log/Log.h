38b7c90193c5f280a9e0962307b23f4f2f909890
GamesTrap <GamesTrap@web.de>
2025-01-25 19:20:25 +0100

    UnitTests Added tests for Profiling.h 01/25/2025 | 25w04b1

--- TRAP/src/Log/Log.h
+++ TRAP/src/Log/Log.h
@@ -151,7 +151,7 @@ namespace TRAP
 		/// @threadsafe
 		void Clear() noexcept;
 
-		static constexpr auto WindowVersion =                        "[25w04a1]";
+		static constexpr auto WindowVersion =                        "[25w04b1]";
 		static constexpr auto WindowPrefix =                         "[Window] ";
 		static constexpr auto WindowIconPrefix =                     "[Window][Icon] ";
 		static constexpr auto ConfigPrefix =                         "[Config] ";
---
+++ UnitTests/src/Core/Profiling.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "TRAP/src/Core/Profiling.h"
+
+TEST_CASE("GetTRAPProfileSystems", "[core][profiling]")
+{
+    [[maybe_unused]] const ProfileSystems profileSystems = GetTRAPProfileSystems();
+}
+
+TEST_CASE("SetTRAPProfileSystems", "[core][profiling]")
+{
+    //Pre condition: Save current value
+    const auto oldValue = GetTRAPProfileSystems();
+
+    SetTRAPProfileSystems(ProfileSystems::All);
+    REQUIRE(GetTRAPProfileSystems() == ProfileSystems::All);
+
+    //Post condition: Reset back to old value
+    SetTRAPProfileSystems(oldValue);
+}
