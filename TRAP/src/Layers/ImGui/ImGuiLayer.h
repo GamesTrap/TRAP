6ede631ff147fec9b178f0c1b77d7108c8d4127d
GamesTrap <GamesTrap@web.de>
2025-03-05 23:45:53 +0100

    Updated ImGuiLayer 03/05/2025 | 25w10a1

--- TRAP/src/Layers/ImGui/ImGuiLayer.h
+++ TRAP/src/Layers/ImGui/ImGuiLayer.h
@@ -85,8 +85,8 @@ namespace ImGui
 	           const TRAP::Graphics::Sampler& sampler,
 	           const ImVec2& size,
 	           const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1),
-			   const ImVec4& tint_col = ImVec4(1, 1, 1, 1),
-			   const ImVec4& border_col = ImVec4(0, 0, 0, 0));
+			   const ImVec4& bg_col = ImVec4(0, 0, 0, 0),
+			   const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
 	/// @brief Draw an image with ImGui.
 	/// @param image Image to draw.
 	/// @param size Size for the image.
@@ -97,8 +97,8 @@ namespace ImGui
 	/// @remark @headless This function is not available in headless mode.
 	void Image(const TRAP::Graphics::Texture& image, const ImVec2& size,
 	           const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1),
-			   const ImVec4& tint_col = ImVec4(1, 1, 1, 1),
-			   const ImVec4& border_col = ImVec4(0, 0, 0, 0));
+			   const ImVec4& bg_col = ImVec4(0, 0, 0, 0),
+			   const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
 
 	/// @brief Draw an image button with ImGui.
 	/// @param image Image to draw.
