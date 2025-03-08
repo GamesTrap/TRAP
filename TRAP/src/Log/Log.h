4be2c239b5efa6267207bc7a380e864ad2f068d3
GamesTrap <GamesTrap@web.de>
2025-03-08 20:26:39 +0100

    Build-CI/NightlyBuild Improved checkout speed of NDA dependencies 03/08/2025 | 25w10a2

--- TRAP/src/Log/Log.h
+++ TRAP/src/Log/Log.h
@@ -151,7 +151,7 @@ namespace TRAP
 		/// @threadsafe
 		void Clear() noexcept;
 
-		static constexpr auto WindowVersion =                        "[25w10a1]";
+		static constexpr auto WindowVersion =                        "[25w10a2]";
 		static constexpr auto WindowPrefix =                         "[Window] ";
 		static constexpr auto WindowIconPrefix =                     "[Window][Icon] ";
 		static constexpr auto ConfigPrefix =                         "[Config] ";
