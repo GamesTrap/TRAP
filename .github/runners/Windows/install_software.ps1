6e0086d4f3f5f0b5dd8d935be68a47d3ac5ae108
GamesTrap <GamesTrap@web.de>
2026-06-23 02:19:49 +0200

    Updated GHA runners to Windows 2025 LTSC with Visual Studio 2026 06/23/2026 | 26w26a1

--- .github/runners/Windows/install_software.ps1
+++ .github/runners/Windows/install_software.ps1
@@ -1,7 +1,7 @@
 choco install -y gh 7zip jq vcredist-all python ninja
 choco install -y git.install --params "'/GitAndUnixToolsOnPath'"
 choco install -y cmake --installargs "ADD_CMAKE_TO_PATH=System"
-choco install -y visualstudio2022buildtools --package-parameters "--includeRecommended --add Microsoft.VisualStudio.Workload.VCTools --locale en-US" #Install Visual Studio 2022 Build Tools (C++ Workload)
+choco install -y visualstudio2026buildtools --package-parameters "--includeRecommended --add Microsoft.VisualStudio.Workload.VCTools --locale en-US" #Install Visual Studio 2026 Build Tools (C++ Workload)
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
 $GitBashPath = ";C:\Program Files\Git\bin;"
